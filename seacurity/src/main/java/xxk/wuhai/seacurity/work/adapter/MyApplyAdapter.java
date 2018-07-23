1c667205569bc39e7b6a06c785214732c915cdcd
869360026@qq.com <869360026@qq.com>
2018-07-23 16:08:21 +0800

    1、修改班次枚举值错误。 2、修改通讯录搜索栏。 3、修改审批以及我的审批页面。

--- seacurity/src/main/java/xxk/wuhai/seacurity/work/adapter/MyApplyAdapter.java
+++ seacurity/src/main/java/xxk/wuhai/seacurity/work/adapter/MyApplyAdapter.java
@@ -51,7 +51,7 @@ public class MyApplyAdapter extends BaseQuickAdapter<ApprovalRecordListBean,Base
             }
         }else{
             helper.setText(R.id.type, item.getPatchTime() != null ? "补签申请" : "请假申请")
-                    .setText(R.id.status, PesonInfoHelper.aplyStatus(item.getStatus()))
+                    .setText(R.id.status, PesonInfoHelper.aplyStatus2(item.getStatus()))
                     .setText(R.id.name, "姓名:" + item.getApName())
                     .setText(R.id.time, item.getPatchTime() != null ? "补签时间：" +
                             item.getPatchTime().replace("-", ".") :
@@ -59,7 +59,7 @@ public class MyApplyAdapter extends BaseQuickAdapter<ApprovalRecordListBean,Base
                                     "-" + item.getLrEndTime().replace("-", "."))
                     .setText(R.id.times, item.getCreateTime())
                     .setText(R.id.reson, "事由：" + (item.getSupplement() == null ? "" : item.getSupplement()));
-            if (PesonInfoHelper.aplyStatus(item.getStatus()).equals("待审核")) {
+            if (PesonInfoHelper.aplyStatus2(item.getStatus()).equals("待审批")) {
                 helper.setTextColor(R.id.status, Color.parseColor("#F43530"));
             } else {
                 helper.setTextColor(R.id.status, Color.parseColor("#49B1FA"));
